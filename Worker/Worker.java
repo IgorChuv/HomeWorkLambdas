48614b72cd5dc655146a9699a44af5ebe68b3f7a
Igor Chuvilov <igorchuv74@gmail.com>
2021-04-07 12:56:15 +0500

    Task 2

---
+++ Worker/Worker.java
@@ -0,0 +1,36 @@
+package Worker;
+
+public class Worker extends Thread {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        int succesTask = 0;
+        int errorTask = 0;
+        for (int i = 1; i < 101; i++) {
+            if (i == 33) {
+                errorCallback.onError("Задача " + i  +" - ошибка выполнения!");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                errorTask ++;
+                continue;
+            }
+            callback.onDone("Задача " + i + " выполнена.");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            succesTask ++;
+        }
+        System.out.println("Успешно выполнено: " + succesTask + " задач. Ошибки выполнения: " + errorTask );
+    }
+}
